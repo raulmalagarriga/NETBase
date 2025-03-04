49665455997edcdcd436b797c83c106e6ce16c6b
Raul Malagarriga <raulmalagarriga@hotmail.com>
2025-03-04 15:33:57 -0400

    FEAT: Data annotations in user model

--- NETBase/Models/User.cs
+++ NETBase/Models/User.cs
@@ -1,21 +1,22 @@
-﻿namespace NETBase.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NETBase.Models
 {
     public class User
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int UserId { get; set; }
+        [StringLength(50)]
         public string Code { get; set; }
+        [StringLength(70)]
         public string Username { get; set; }
+        [StringLength(50)]
         public string Email { get; set; }
+        [StringLength(70)]
         public string Password { get; set; }
         public DateTime CreatedAt { get; set; } = System.DateTime.UtcNow;
-
-        public User(User user)
-        {
-            Code = user.Code;
-            Username = user.Username;
-            Email = user.Email;
-            Password = user.Password;
-        }
     }
 
 
