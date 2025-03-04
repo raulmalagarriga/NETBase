ed6a8136e5c68d46869b9a1942589714a2631ed1
Raul Malagarriga <raulmalagarriga@hotmail.com>
2025-03-04 15:17:28 -0400

    FEAT: Main configuration for data base connection

---
+++ NETBase/Data/DataDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NETBase.Models;
+
+namespace NETBase.Data
+{
+    public class DataDBContext: DbContext
+    {
+        public DataDBContext(DbContextOptions options) : base(options)
+        { }
+        public virtual DbSet<User> User { get; set; }
+
+        public DataDBContext CreateConnection(string connectionString)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DataDBContext>();
+            optionsBuilder.UseNpgsql(connectionString);
+            var context = new DataDBContext(optionsBuilder.Options);
+            return context;
+        }
+    }
+}
--- NETBase/Program.cs
+++ NETBase/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using NETBase.Data;
 using NETBase.Interfaces.IRepositories;
 using NETBase.Interfaces.IServices;
 using NETBase.Repositories;
@@ -8,15 +10,29 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// DATABASE CONN
+builder.Services.AddDbContext<DataDBContext>(options => options.UseNpgsql(
+    builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // SERVICES
 builder.Services.AddTransient<IUserService, UserService>();
 
 // REPOSITORIES
 builder.Services.AddTransient<IUserRepository, UserRepository>();
+
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", builder =>
+    {
+        builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -25,6 +41,13 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+// ENSURE THAT DATABASE IS CREATED IF NOT CREATE IT
+using (var scope = app.Services.CreateScope())
+{
+    var Context = scope.ServiceProvider.GetRequiredService<DataDBContext>();
+    Context.Database.EnsureCreated();
 }
 
 app.UseHttpsRedirection();
