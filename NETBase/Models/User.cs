8301ad570c3cd6d4952ad1c903b4dfe1e2821334
Raul Malagarriga <raulmalagarriga@hotmail.com>
2025-03-04 14:04:03 -0400

    FEAT: First commit and first configuration project

---
+++ NETBase/Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace NETBase.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Code { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedAt { get; set; } = System.DateTime.UtcNow;
+
+        public User(User user)
+        {
+            Code = user.Code;
+            Username = user.Username;
+            Email = user.Email;
+            Password = user.Password;
+        }
+    }
+
+
+}
