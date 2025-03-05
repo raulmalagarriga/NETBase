df00e12c621846f0e1e7a796e9443ce6cd9bbe21
Raul Malagarriga <raulmalagarriga@hotmail.com>
2025-03-05 19:26:03 -0400

    FEAT: User repository layer done

--- NETBase/Interfaces/IRepositories/IUserRepository.cs
+++ NETBase/Interfaces/IRepositories/IUserRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace NETBase.Interfaces.IRepositories
+﻿using NETBase.Models;
+
+namespace NETBase.Interfaces.IRepositories
 {
     public interface IUserRepository
     {
+        Task<List<User>> GetUsers();
+        Task<User> GetUserByCode(string code);
+        void CreateUser(User data);
+        void UpdateUser(User data);
     }
 }
--- NETBase/Repositories/UserRepository.cs
+++ NETBase/Repositories/UserRepository.cs
@@ -1,8 +1,39 @@
-﻿using NETBase.Interfaces.IRepositories;
+﻿using Microsoft.EntityFrameworkCore;
+using NETBase.Data;
+using NETBase.Interfaces.IRepositories;
+using NETBase.Models;
 
 namespace NETBase.Repositories
 {
-    public class UserRepository:IUserRepository
+    public class UserRepository : IUserRepository
     {
+        private readonly DataDBContext dBContext;
+        public UserRepository(DataDBContext dBContext)
+        {
+            this.dBContext = dBContext;
+        }
+
+        public void CreateUser(User data)
+        {
+            dBContext.User.Add(data);
+            dBContext.SaveChanges();
+        }
+
+        public async Task<User> GetUserByCode(string code)
+        {
+            return await dBContext.User.FirstOrDefaultAsync(u => u.Code == code);
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            return await dBContext.User.ToListAsync();
+        }
+
+        public void UpdateUser(User data)
+        {
+            User userToUpdate = dBContext.User.FirstOrDefault();
+            if (userToUpdate != null) userToUpdate = data;
+            dBContext.SaveChanges();
+        }
     }
 }
