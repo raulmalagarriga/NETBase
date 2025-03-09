2b7f242be89d7c6ac478bec8c2b940fa99189411
Raul Malagarriga <raulmalagarriga@hotmail.com>
2025-03-09 12:21:29 -0400

    FEAT: Service layer interface implemented

--- NETBase/Interfaces/IServices/IUserService.cs
+++ NETBase/Interfaces/IServices/IUserService.cs
@@ -1,6 +1,13 @@
-﻿namespace NETBase.Interfaces.IServices
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NETBase.Interfaces.IServices
 {
     public interface IUserService
     {
+        Task<IActionResult> GetUsers();
+        Task<IActionResult> GetUserByCode();
+        Task<IActionResult> CreateUser();
+        Task<IActionResult> UpdateUser();
+        Task<IActionResult> DeleteUser();
     }
 }
--- NETBase/Services/UserService.cs
+++ NETBase/Services/UserService.cs
@@ -1,8 +1,33 @@
-﻿using NETBase.Interfaces.IServices;
+﻿using Microsoft.AspNetCore.Mvc;
+using NETBase.Interfaces.IServices;
 
 namespace NETBase.Services
 {
-    public class UserService:IUserService
+    public class UserService : IUserService
     {
+        public Task<IActionResult> CreateUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> DeleteUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> GetUserByCode()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> UpdateUser()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
